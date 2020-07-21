15035644573be1d7bd9acd9bc297a03f021be65e
KogaYamada <koga0087y@gmail.com>
2020-07-21 21:50:47 +0900

    enum

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('hi there');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+// enum =>enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Drow = 'D',
+}
+
+const prontMatchResult = (match) => {
+  if (match[5] === 'H') {
+    return MatchResult.HomeWin;
+  }
+  return MatchResult.AwayWin;
+};
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(manUnitedWins);
